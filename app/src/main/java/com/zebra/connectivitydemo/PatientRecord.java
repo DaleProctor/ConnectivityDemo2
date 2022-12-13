27e836d7601487cefd6044977bd4a07d23e61965
Dale Proctor <dfproctor@gmail.com>
2022-12-13 11:06:06 -0800

    Initial commit of wristband project

---
+++ app/src/main/java/com/zebra/connectivitydemo/PatientRecord.java
@@ -0,0 +1,12 @@
+package com.zebra.connectivitydemo;
+
+public class PatientRecord {
+        public String m_id;
+        public String m_first;
+        public String m_middle;
+        public String m_father;
+        public String m_mother;
+        public String m_dob;
+        public String m_gender;
+        public String m_curp;
+    }
